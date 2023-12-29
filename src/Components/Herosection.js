98ef9af74fb10fccc4e8a925091f9437c99e9625
Zachary Jacobs <zprogrammercode@gmail.com>
2023-12-28 17:36:19 -0700

    got the drawing in

--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 
 const Herosection = () => {
 
-    return(
+    return(<>
+       
         <div className="hero-section">
-        <img src="https://res.cloudinary.com/dlnshpfdv/image/upload/v1703703281/Jacobs_Brothers_Moving_asnd_storage_w2xnkg.png" alt="da-logo" className="hero-pic"/>
         <div className="hero-info">
-            <Button className="hero-info-button"> Get Pricing </Button>
-    
+       
+        <img src="https://res.cloudinary.com/dlnshpfdv/image/upload/v1703809226/Untitled_drawing_2_2_jfb87c.png" alt="drawing" className="hero-image"/>
+      
         </div>
         </div>
+        </>
     )
 }
 
 export default Herosection;
+
+/*        <img src="https://res.cloudinary.com/dlnshpfdv/image/upload/v1703703281/Jacobs_Brothers_Moving_asnd_storage_w2xnkg.png" alt="da-logo" className="hero-pic"/>
+*/
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,38 @@
 import React from "react";
-import { Navbar, Container} from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 
 const Jbnavbar = () => {
 
 return(
 <>
-<Navbar >
-<Container className="navbar">
-  <Navbar.Brand href="#home" className="logo">
-    <img
-      alt=""
-      src="https://res.cloudinary.com/dlnshpfdv/image/upload/v1703784466/Jacobs_Brothers_Moving_asnd_storage_3_dvfifr.png"
-      width="180"
-      height="180"
-      className="brand"
-    />{' '}
-    <div className="title-center">
-    </div>
-  </Navbar.Brand>
-  </Container>
+<Navbar className="navbar"  >
+
+
+<Navbar.Brand href="#home" className="logo">
+  
+  <img
+    alt=""
+    src="https://res.cloudinary.com/dlnshpfdv/image/upload/v1703808276/Untitled_drawing_3_bg3ea2.png"
+    width="130"
+    height="130"
+    className="brand"
+  />{' '}
+  <div className="title-center">
+  </div>
+</Navbar.Brand>
+    <div className="phone">719-338-4333</div>
+    
+ 
+       
+  <Nav className="nav-links">
+            <Nav.Link href="/house" className="each-link"> House </Nav.Link>
+            <Nav.Link  href="/Apartment" className="each-link"> Apartment </Nav.Link>
+            <Nav.Link href="/Condo" className="each-link"> Condo </Nav.Link>
+            <Nav.Link href="#" className="each-link"> Storage </Nav.Link>
+
+                      </Nav>
+ 
  
 </Navbar>
 </>
